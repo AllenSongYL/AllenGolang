40bceb59c9a6f42e39ad5c2e19bb4a60dbe14465
AllenSongYL <1171951419@qq.com>
2021-09-29 21:01:57 +0800

    change

--- CurlAddInputs/AutoInputs.go
+++ CurlAddInputs/AutoInputs.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"time"
 )
 
 // 执行linux命令
@@ -20,30 +21,68 @@ func exec_shell(s string) (string, error) {
 
 func exec_curl(ips, ipType string) {
 
-	// 测试环境
-	centosCommand := "curl -k -u admin:'1qaz!QAZ' http://192.168.220.110:8089/services/data/inputs/udp/514 " +
-		"-d \"restrictToHost=" + ips + "&index=crcb_unix&sourcetype=centos_unix\""
+	// own测试环境
+	CurlCommand := "curl -k -u admin:'1qaz!QAZ' https://192.168.220.110:8089/services/data/inputs/udp/514 " +
+		"-d \"restrictToHost=" + ips + "&index=crcb_unix&sourcetype="
+
+	// 农商行测试环境
+	// CurlCommand := "curl -k -u admin:'1qaz!QAZ' https://170.130.106.23:8089/services/data/inputs/udp/514 " +
+	//	"-d \"restrictToHost=" + ips + "&index=crcb_unix&sourcetype="
+
 	// 生产环境命令
-	//centosCommand := "curl -k -u admin:'1qaz!QAZ' https://170.130.106.23:8089/services/data/inputs/udp/514 " +
-	//	"-d \"restrictToHost=" + ips + "&index=crcb_unix&sourcetype=centos_unix\""
+	//CurlCommand := "curl -k -u input:'1234qwer' https://10.13.130.21:8089/services/data/inputs/udp/514 " +
+	//	"-d \"restrictToHost=" + ips + "&index=crcb_unix&sourcetype="
 
 	if ipType == "CENTOS" {
-		results, err := exec_shell(centosCommand)
+		fmt.Println("开始添加centos_unix：", ips)
+		_, err := exec_shell(CurlCommand + "centos_unix\"")
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal("执行curl命令失败：", err)
 		} else {
-			fmt.Println(results)
+			fmt.Println("添加成功！")
+		}
+	} else if ipType == "REDHAT" {
+		fmt.Println("开始添加redhat_unix：", ips)
+		_, err := exec_shell(CurlCommand + "redhat_unix\"")
+		if err != nil {
+			log.Fatal("执行curl命令失败：", err)
+		} else {
+			fmt.Println("添加成功！")
+		}
+	} else if ipType == "AIX" {
+		fmt.Println("开始添加aix_unix：", ips)
+		_, err := exec_shell(CurlCommand + "aix_unix\"")
+		if err != nil {
+			log.Fatal("执行curl命令失败：", err)
+		} else {
+			fmt.Println("添加成功！")
+		}
+	} else {
+		fmt.Println("开始添加suse_unix：", ips)
+		_, err := exec_shell(CurlCommand + "suse_unix\"")
+		if err != nil {
+			log.Fatal("执行curl命令失败：", err)
+		} else {
+			fmt.Println("添加成功！")
 		}
 	}
-
 }
 
 func main() {
-	// 打开csv文件
-	csvName := "G:\\GO\\笔记\\AllenGolang\\CurlAddInputs\\csvDir\\ip_assets.csv"
+
+	// 测试环境 打开csv文件
+	//csvName := "G:\\GO\\笔记\\AllenGolang\\CurlAddInputs\\csvDir\\ip_assets.csv"
+	csvName := "/root/AutoInputs/ip_assets.csv"
+
+	timeStart := time.Now()
+	timeStartFormat := timeStart.Format("2006-01-02 15:04:05")
+	fmt.Println("程序开始时间：", timeStartFormat)
+
 	csvFile, err := os.Open(csvName)
 	if err != nil {
 		log.Fatal("无法打开该文件", err)
+	} else {
+		fmt.Println("成功打开文件：", csvName)
 	}
 	defer csvFile.Close()
 
@@ -53,9 +92,18 @@ func main() {
 	record, err := r.ReadAll()
 	if err != nil {
 		log.Fatal(err)
+	} else {
+		fmt.Println("成功解析文件：", csvName)
 	}
 
 	for _, values := range record {
 		exec_curl(values[0], values[1])
 	}
+
+	// 程序结束输出
+	timeEnd := time.Now()
+	timeEndFormat := timeEnd.Format("2006-01-02 15:04:05")
+	fmt.Println("结束时间：", timeEndFormat)
+	timeSub := timeEnd.Sub(timeStart)
+	fmt.Println("运行时长：", timeSub)
 }
